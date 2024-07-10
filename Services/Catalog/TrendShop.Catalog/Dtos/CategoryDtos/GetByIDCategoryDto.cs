66b4d4d3df0fa2d85f89625474dc1db6be472449
Haçik Ulu <hacikd2@gmail.com>
2024-07-10 21:16:52 +0300

    Dtos adding.

---
+++ Services/Catalog/TrendShop.Catalog/Dtos/CategoryDtos/GetByIDCategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TrendShop.Catalog.Dtos.CategoryDtos
+{
+    public class GetByIDCategoryDto
+    {
+        public string CategoryID { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
---
+++ Services/Catalog/TrendShop.Catalog/Dtos/CategoryDtos/UpdateCategoryName.cs
@@ -0,0 +1,8 @@
+﻿namespace TrendShop.Catalog.Dtos.CategoryDtos
+{
+    public class UpdateCategoryName
+    {
+        public string CategoryID { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
