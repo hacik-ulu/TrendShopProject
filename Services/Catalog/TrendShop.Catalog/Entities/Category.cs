6b342e70d87760b3059cb8e30a4373ea9bd334cc
Haçik Ulu <hacikd2@gmail.com>
2024-07-10 21:05:40 +0300

    Entities adding.

--- Services/Catalog/TrendShop.Catalog/Entities/Category.cs
+++ Services/Catalog/TrendShop.Catalog/Entities/Category.cs
@@ -1,6 +1,13 @@
-﻿namespace TrendShop.Catalog.Entities
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace TrendShop.Catalog.Entities
 {
+    // MongoDB doesn't have any relational database and ID's are kept by string.
     public class Category
     {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string CategoryID { get; set; }
+        public string CategoryName { get; set; }
     }
 }
