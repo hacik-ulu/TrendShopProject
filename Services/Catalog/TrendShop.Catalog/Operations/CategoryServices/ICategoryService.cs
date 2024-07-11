c4830936ae98ff8251bfd2eb2d321b417a33f959
Haçik Ulu <hacikd2@gmail.com>
2024-07-11 21:15:23 +0300

    Category Services edited.

---
+++ Services/Catalog/TrendShop.Catalog/Operations/CategoryServices/ICategoryService.cs
@@ -0,0 +1,14 @@
+﻿using TrendShop.Catalog.Dtos.CategoryDtos;
+
+namespace TrendShop.Catalog.Operations.CategoryServices
+{
+    public interface ICategoryService
+    {
+        Task<List<ResultCategoryDto>> GetAllCategoryAsync();
+        Task CreateCategoryAsync(CreateCategoryDto createCategoryDto);
+        Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto);
+        Task DeleteCategoryAsync(string id);
+        Task<GetByIdCategoryDto> GetByIdCategoryAsync(string id);
+
+    }
+}
