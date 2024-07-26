3a55b82556b10f683f86c147f50b196db89b9e2d
Haçik Ulu <hacikd2@gmail.com>
2024-07-26 16:26:02 +0300

    Program.cs edited.

--- Services/Catalog/TrendShop.Catalog/Program.cs
+++ Services/Catalog/TrendShop.Catalog/Program.cs
@@ -1,6 +1,30 @@
+using Microsoft.Extensions.Options;
+using System.Reflection;
+using TrendShop.Catalog.Operations.CategoryServices;
+using TrendShop.Catalog.Operations.ProductDetailDetailServices;
+using TrendShop.Catalog.Operations.ProductDetailServices;
+using TrendShop.Catalog.Operations.ProductImageImageServices;
+using TrendShop.Catalog.Operations.ProductImageServices;
+using TrendShop.Catalog.Operations.ProductServices;
+using TrendShop.Catalog.Settings;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IProductDetailService, ProductDetailService>();
+builder.Services.AddScoped<IProductImageService, ProductImageService>();
+
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
+
+// sp --> service provider ile DatabaseSettings deðerlerine ulaþacak.
+builder.Services.AddScoped<IDatabaseSettings>(sp =>
+{
+    return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+});
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
