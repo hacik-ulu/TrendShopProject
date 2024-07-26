cd4ed48070cf69fef5a8f76c3edf4eed77c417a5
Haçik Ulu <hacikd2@gmail.com>
2024-07-26 16:05:36 +0300

    Controllers edited.

---
+++ Services/Catalog/TrendShop.Catalog/Controllers/ProductDetailsController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TrendShop.Catalog.Dtos.ProductDetailDtos;
+using TrendShop.Catalog.Operations.ProductDetailDetailServices;
+
+namespace TrendShop.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductDetailsController : ControllerBase
+    {
+        private readonly IProductDetailService _ProductDetailService;
+
+        public ProductDetailsController(IProductDetailService ProductDetailService)
+        {
+            _ProductDetailService = ProductDetailService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ProductDetailList()
+        {
+            var values = await _ProductDetailService.GetAllProductDetailAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductDetailById(string id)
+        {
+            var values = await _ProductDetailService.GetByIdProductDetailAsync(id);
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProductDetail(CreateProductDetailDto createProductDetailDto)
+        {
+            await _ProductDetailService.CreateProductDetailAsync(createProductDetailDto);
+            return Ok("Ürün Detayı Eklendi!");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteProductDetail(string id)
+        {
+            var values = await _ProductDetailService.GetByIdProductDetailAsync(id);
+            return Ok("Ürün Detayı Silindi!");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateProductDetail(UpdateProductDetailDto updateProductDetailDto)
+        {
+            await _ProductDetailService.UpdateProductDetailAsync(updateProductDetailDto);
+            return Ok("Ürün Detayı Başarıyla Güncellendi");
+        }
+    }
+}
---
+++ Services/Catalog/TrendShop.Catalog/Controllers/ProductImagesController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TrendShop.Catalog.Dtos.ProductImageDtos;
+using TrendShop.Catalog.Operations.ProductImageServices;
+using TrendShop.Catalog.Operations.ProductImageImageServices;
+
+namespace TrendShop.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductImagesController : ControllerBase
+    {
+        private readonly IProductImageService _ProductImageService;
+
+        public ProductImagesController(IProductImageService ProductImageService)
+        {
+            _ProductImageService = ProductImageService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ProductImageList()
+        {
+            var values = await _ProductImageService.GetAllProductImageAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductImageById(string id)
+        {
+            var values = await _ProductImageService.GetByIdProductImageAsync(id);
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProductImage(CreateProductImageDto createProductImageDto)
+        {
+            await _ProductImageService.CreateProductImageAsync(createProductImageDto);
+            return Ok("Ürün Görseli Eklendi!");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteProductImage(string id)
+        {
+            var values = await _ProductImageService.GetByIdProductImageAsync(id);
+            return Ok("Ürün Görseli Silindi!");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateProductImage(UpdateProductImageDto updateProductImageDto)
+        {
+            await _ProductImageService.UpdateProductImageAsync(updateProductImageDto);
+            return Ok("Ürün Görseli Başarıyla Güncellendi");
+        }
+    }
+}
