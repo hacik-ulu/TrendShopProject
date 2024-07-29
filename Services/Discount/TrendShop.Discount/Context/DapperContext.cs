2606ca95e25c11572be938f1cf181028ae382c90
Haçik Ulu <hacikd2@gmail.com>
2024-07-29 12:15:47 +0300

    Discount services editing.

---
+++ Services/Discount/TrendShop.Discount/Context/DapperContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+using TrendShop.Discount.Entities;
+
+namespace TrendShop.Discount.Context
+{
+    public class DapperContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public DapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("DefaultConnection");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=HACIKULU\\SQLEXPRESS;Initial Catalog =TrendShopCatalogDb;integrated Security=true");
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+
+        // Dapper üzerinden bağlantı kurmak için.
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+    }
+}
