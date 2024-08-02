caf6400e00ae7b1bcb3f013dc24e7d3c294bfffe
Haçik Ulu <hacikd2@gmail.com>
2024-08-02 18:22:23 +0300

    Admin and visitor client configurations edited.

--- IdentityServer/TrendShop.IdentityServer/Config.cs
+++ IdentityServer/TrendShop.IdentityServer/Config.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 using static System.Formats.Asn1.AsnWriter;
@@ -50,7 +51,36 @@ namespace TrendShop.IdentityServer
                 AllowedGrantTypes= GrantTypes.ClientCredentials,
                 ClientSecrets = {new Secret("trendshopsecret".Sha256())},
                 AllowedScopes = {"CatalogReadPermission"}
-            }
+            },
+
+            // Manager
+            new Client
+            {
+                ClientId = "TrendShopManagerId",
+                ClientName = "Trend Shop Manager User",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = {new Secret("trendshopsecret".Sha256())},
+                AllowedScopes = {"CatalogFullPermission" }
+            },
+
+            //Admin
+            new Client
+            {
+                ClientId = "TrendShopAdminId",
+                ClientName = "Trend Shop Admin User",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = {new Secret("trendshopsecret".Sha256())},
+                AllowedScopes = {"CatalogFullPermission","CatalogReadPermision", "DiscountFullPermission", "OrderFullPermission",
+                IdentityServerConstants.LocalApi.ScopeName,
+
+                // Admin kullanıcı bilgilerine erişiyor.
+                IdentityServerConstants.StandardScopes.Email,
+                IdentityServerConstants.StandardScopes.OpenId,
+                IdentityServerConstants.StandardScopes.Profile,
+                },
+
+                AccessTokenLifetime = 600
+            },
         };
     }
 }
