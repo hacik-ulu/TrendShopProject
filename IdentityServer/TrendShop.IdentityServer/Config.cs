52c3819bb9fd00ba811fc52cebb1f96bcb75ba62
Haçik Ulu <hacikd2@gmail.com>
2024-08-02 17:33:51 +0300

    Auth is editing.

--- IdentityServer/TrendShop.IdentityServer/Config.cs
+++ IdentityServer/TrendShop.IdentityServer/Config.cs
@@ -12,7 +12,9 @@ namespace TrendShop.IdentityServer
     {
         public static IEnumerable<ApiResource> ApiResources => new ApiResource[]
         {
-           new ApiResource("ResourceCatalog"){Scopes = {"CatalogFullPermission","CatalogReadPermission" } }
+           new ApiResource("ResourceCatalog"){Scopes = {"CatalogFullPermission","CatalogReadPermission" } },
+           new ApiResource("ResourceDiscount"){Scopes = { "DiscountFullPermission" } },
+           new ApiResource("OrderDiscount"){Scopes = { "OrderFullPermission" } }
 
         };
 
@@ -27,7 +29,9 @@ namespace TrendShop.IdentityServer
         public static IEnumerable<ApiScope> ApiScopes => new ApiScope[]
         {
             new ApiScope("CatalogFullPermission","Full authority for catalog operations"),
-            new ApiScope("CatalogReadPermission","Reading authority for catalog operations")
+            new ApiScope("CatalogReadPermission","Reading authority for catalog operations"),
+            new ApiScope("DiscountFullPermission","Full authority for discount operations"),
+            new ApiScope("OrderFullPermission","Full authority for order operations")
         };
     }
 }
