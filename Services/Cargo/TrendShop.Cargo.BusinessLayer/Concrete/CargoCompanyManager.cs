7974dbd1be0e46ef12995befd02c04f73e77212f
Haçik Ulu <hacikd2@gmail.com>
2024-08-08 18:27:32 +0300

    Business layer editing.

---
+++ Services/Cargo/TrendShop.Cargo.BusinessLayer/Concrete/CargoCompanyManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrendShop.Cargo.BusinessLayer.Abstract;
+using TrendShop.Cargo.EntityLayer.Concrete;
+
+namespace TrendShop.Cargo.BusinessLayer.Concrete
+{
+    public class CargoCompanyManager : ICargoCompanyService
+    {
+
+        public void TAdd(CargoCompany entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CargoCompany> TGetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public CargoCompany TGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(CargoCompany entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/Cargo/TrendShop.Cargo.BusinessLayer/Concrete/CargoDetailManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrendShop.Cargo.BusinessLayer.Abstract;
+using TrendShop.Cargo.DataAccessLayer.Abstract;
+using TrendShop.Cargo.EntityLayer.Concrete;
+
+namespace TrendShop.Cargo.BusinessLayer.Concrete
+{
+    public class CargoDetailManager : ICargoDetailService
+    {
+        private readonly ICargoDetailDal _cargoDetailDal;
+
+        public CargoDetailManager(ICargoDetailDal cargoDetailDal)
+        {
+            _cargoDetailDal = cargoDetailDal;
+        }
+
+        public void TAdd(CargoDetail entity)
+        {
+            _cargoDetailDal.Add(entity);
+        }
+
+        public void TDelete(int id)
+        {
+            _cargoDetailDal.Delete(id);
+        }
+
+        public List<CargoDetail> TGetAll()
+        {
+            return _cargoDetailDal.GetAll();
+        }
+
+        public CargoDetail TGetById(int id)
+        {
+            return _cargoDetailDal.GetById(id);
+        }
+
+        public void TUpdate(CargoDetail entity)
+        {
+            _cargoDetailDal.Update(entity);
+        }
+    }
+}
