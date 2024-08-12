136504898e6999a9d9efb6212c5229de9778f8cc
Haçik Ulu <hacikd2@gmail.com>
2024-08-12 16:50:16 +0300

    BasketService is editing.

---
+++ Services/Basket/TrendShop.Basket/Services/BasketService.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+using TrendShop.Basket.Dtos;
+using TrendShop.Basket.Settings;
+
+namespace TrendShop.Basket.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly RedisService _redisService;
+
+        public BasketService(RedisService redisService)
+        {
+            _redisService = redisService;
+        }
+
+        public async Task DeleteBasket(string userId)
+        {
+            await _redisService.GetDb().KeyDeleteAsync(userId);
+        }
+
+        public async Task<BasketTotalDto> GetBasketTotal(string userId)
+        {
+            var existBasket = await _redisService.GetDb().StringGetAsync(userId);
+            // existBasketten gelen json verilerini BasketTotalDto'ya dönüştürür, deserilize eder.
+            return JsonSerializer.Deserialize<BasketTotalDto>(existBasket);
+        }
+
+        public async Task SaveBasket(BasketTotalDto basketTotalDto)
+        {
+            await _redisService.GetDb().StringSetAsync(basketTotalDto.UserID, JsonSerializer.Serialize(basketTotalDto));
+        }
+    }
+}
---
+++ Services/Basket/TrendShop.Basket/Services/IBasketService.cs
@@ -0,0 +1,11 @@
+﻿using TrendShop.Basket.Dtos;
+
+namespace TrendShop.Basket.Services
+{
+    public interface IBasketService
+    {
+        Task<BasketTotalDto> GetBasketTotal(string userId);
+        Task SaveBasket(BasketTotalDto basketTotalDto);
+        Task DeleteBasket(string userId);
+    }
+}
