fadc9ad1721a475c797f47d79d837605793546dd
Haçik Ulu <hacikd2@gmail.com>
2024-08-18 14:39:39 +0300

    ProductList editing.

---
+++ Frontends/TrendShop.WebUI/ViewComponents/ProductListViewComponents/_ProductListColorFilterComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrendShop.WebUI.ViewComponents.ProductListViewComponents
+{
+    public class _ProductListColorFilterComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
