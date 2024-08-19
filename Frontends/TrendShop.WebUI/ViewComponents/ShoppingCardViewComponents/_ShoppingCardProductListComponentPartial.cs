9eee825e20cc85b5e6bebba19688acfbb83480bf
Haçik Ulu <hacikd2@gmail.com>
2024-08-19 21:17:49 +0300

    Shopping Card edited.

---
+++ Frontends/TrendShop.WebUI/ViewComponents/ShoppingCardViewComponents/_ShoppingCardProductListComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrendShop.WebUI.ViewComponents.ShoppingCardViewComponents
+{
+    public class _ShoppingCardProductListComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
