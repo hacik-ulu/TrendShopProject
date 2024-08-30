96f3cceb21294a900266ffad86d7dceb3168d7db
Haçik Ulu <hacikd2@gmail.com>
2024-08-30 20:06:34 +0300

    Categories editing.

--- Frontends/TrendShop.WebUI/ViewComponents/DefaultViewComponents/_CategoriesDefaultComponentPartial.cs
+++ Frontends/TrendShop.WebUI/ViewComponents/DefaultViewComponents/_CategoriesDefaultComponentPartial.cs
@@ -1,11 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TrendShop.DtoLayer.CatalogDtos.CategoryDtos;
 
 namespace TrendShop.WebUI.ViewComponents.DefaultViewComponents
 {
     public class _CategoriesDefaultComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _CategoriesDefaultComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7207/api/Categories");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return View(values);
+            }
+
             return View();
         }
     }
