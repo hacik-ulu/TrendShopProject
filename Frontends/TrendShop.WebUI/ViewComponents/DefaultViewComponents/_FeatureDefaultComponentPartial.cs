6b6cd1c42f69bac961a3f464a7b99c4b35803650
Haçik Ulu <hacikd2@gmail.com>
2024-08-30 19:21:47 +0300

    Feature edited in UI.

--- Frontends/TrendShop.WebUI/ViewComponents/DefaultViewComponents/_FeatureDefaultComponentPartial.cs
+++ Frontends/TrendShop.WebUI/ViewComponents/DefaultViewComponents/_FeatureDefaultComponentPartial.cs
@@ -1,11 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TrendShop.DtoLayer.CatalogDtos.FeatureDtos;
 
 namespace TrendShop.WebUI.ViewComponents.DefaultViewComponents
 {
     public class _FeatureDefaultComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _FeatureDefaultComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7207/api/Features");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFeatureDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
