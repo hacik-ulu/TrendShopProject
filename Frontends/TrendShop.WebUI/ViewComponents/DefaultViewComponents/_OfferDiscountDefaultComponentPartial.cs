5feaf4584afe14d9573682374f8cbf0b5029157b
Haçik Ulu <hacikd2@gmail.com>
2024-09-07 19:43:10 +0300

    OfferDiscount edited.

--- Frontends/TrendShop.WebUI/ViewComponents/DefaultViewComponents/_OfferDiscountDefaultComponentPartial.cs
+++ Frontends/TrendShop.WebUI/ViewComponents/DefaultViewComponents/_OfferDiscountDefaultComponentPartial.cs
@@ -1,11 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TrendShop.DtoLayer.CatalogDtos.OfferDiscountDtos;
 
 namespace TrendShop.WebUI.ViewComponents.DefaultViewComponents
 {
     public class _OfferDiscountDefaultComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _OfferDiscountDefaultComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7207/api/OfferDiscounts");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultOfferDiscountDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
