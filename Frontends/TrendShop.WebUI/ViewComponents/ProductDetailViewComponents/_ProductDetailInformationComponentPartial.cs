0b56006c956fefd94c36582e15e207c27216e17f
Haçik Ulu <hacikd2@gmail.com>
2024-10-26 00:12:10 +0300

    Editing.

--- Frontends/TrendShop.WebUI/ViewComponents/ProductDetailViewComponents/_ProductDetailInformationComponentPartial.cs
+++ Frontends/TrendShop.WebUI/ViewComponents/ProductDetailViewComponents/_ProductDetailInformationComponentPartial.cs
@@ -1,12 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TrendShop.DtoLayer.CatalogDtos.ProductDetailDtos;
 
 namespace TrendShop.WebUI.ViewComponents.ProductDetailViewComponents
 {
     public class _ProductDetailInformationComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _ProductDetailInformationComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7207/api/ProductDetails/GetProductDetailByProductId?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateProductDetailDto>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
 }
+
+// Tüm ürünlerin görsellerini yükle.
+// Comment Mikroservis --> 216.video.
