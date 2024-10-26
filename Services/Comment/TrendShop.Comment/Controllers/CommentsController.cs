b882fb42e9dddd31057ddbecd7bc907bd6e88f0b
Haçik Ulu <hacikd2@gmail.com>
2024-10-26 18:00:24 +0300

    CommentsController edited.

---
+++ Services/Comment/TrendShop.Comment/Controllers/CommentsController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TrendShop.Comment.Context;
+using TrendShop.Comment.Entities;
+
+namespace TrendShop.Comment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [AllowAnonymous]
+    public class CommentsController : ControllerBase
+    {
+        private readonly CommentContext _context;
+
+        public CommentsController(CommentContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult CommentList()
+        {
+            var values = _context.UserComments.ToList();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateComment(UserComment userComment)
+        {
+            _context.UserComments.Add(userComment);
+            _context.SaveChanges();
+            return Ok("Yorum başarıyla eklendi");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteComment(int id)
+        {
+            var value = _context.UserComments.Find(id);
+            _context.UserComments.Remove(value);
+            _context.SaveChanges();
+            return Ok("Yorum başarıyla silindi!");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetComment(int id)
+        {
+            var value = _context.UserComments.Find(id);
+            return Ok(value);
+        }
+
+
+        [HttpPut]
+        public IActionResult UpdateComment(UserComment userComment)
+        {
+            _context.UserComments.Update(userComment);
+            _context.SaveChanges();
+            return Ok("Yorum başarıyla güncellendi");
+        }
+    }
+}
