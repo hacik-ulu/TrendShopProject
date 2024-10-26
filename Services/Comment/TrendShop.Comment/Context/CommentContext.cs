604e3eb8063673e8b63ba24bdfb47c949a9894d3
Haçik Ulu <hacikd2@gmail.com>
2024-10-26 17:39:30 +0300

    CommentContext editing.

---
+++ Services/Comment/TrendShop.Comment/Context/CommentContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TrendShop.Comment.Entities;
+
+namespace TrendShop.Comment.Context
+{
+    public class CommentContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public CommentContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = _configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        public DbSet<UserComment> UserComments { get; set; }
+    }
+}
