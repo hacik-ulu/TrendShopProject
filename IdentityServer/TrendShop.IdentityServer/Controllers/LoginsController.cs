95f8ed1eb89161206c8cc236c1422b442672837e
Haçik Ulu <hacikd2@gmail.com>
2024-10-30 21:32:33 +0300

    Login api edited.

---
+++ IdentityServer/TrendShop.IdentityServer/Controllers/LoginsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TrendShop.IdentityServer.Dtos;
+using TrendShop.IdentityServer.Models;
+
+
+namespace TrendShop.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginsController : ControllerBase
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        public LoginsController(SignInManager<ApplicationUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UserLogin(UserLoginDto userLoginDto)
+        {
+            var result = await _signInManager.PasswordSignInAsync(userLoginDto.Username, userLoginDto.Password, false, false);
+            if (result.Succeeded)
+            {
+                return Ok("Giriş Başarılı");
+            }
+            else
+            {
+                return Ok("Email veya Şifre Hatalı");
+            }
+        }
+
+    }
+}
