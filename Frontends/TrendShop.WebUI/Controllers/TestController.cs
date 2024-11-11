18ec67ab0570c11422b0142fe61f39c95942cf1a
Haçik Ulu <hacikd2@gmail.com>
2024-11-11 21:49:22 +0300

    Authorization edited.

---
+++ Frontends/TrendShop.WebUI/Controllers/TestController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using TrendShop.DtoLayer.CatalogDtos.CategoryDtos;
+
+namespace TrendShop.WebUI.Controllers
+{
+    public class TestController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public TestController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+
+            // kullanıcı türünü belirlemek ve access token almak için api'ye post ediyoruz.
+            // tokenı UI'da göüntülemeye çalışıyoruz.
+            string token;
+            using (var httpClient = new HttpClient())
+            {
+                var request = new HttpRequestMessage
+                {
+                    RequestUri = new Uri("http://localhost:5001/connect/token"),
+                    Method = HttpMethod.Post,
+                    Content = new FormUrlEncodedContent(new Dictionary<string, string>
+                    {
+                        {"client_id","TrendShopVisitorId" },
+                        {"client_secret","trendshopsecret" },
+                        {"grant_type","client_credentials" },
+                    })
+                };
+
+                using (var response = await httpClient.SendAsync(request))
+                {
+                    if (response.IsSuccessStatusCode) 
+                    {
+                        var content = await response.Content.ReadAsStringAsync();
+                        var tokenResponse = JObject.Parse(content);
+                        token = tokenResponse["access_token"].ToString();
+                    }
+                };
+            }
+
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7207/api/Categories");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return View(values);
+            }
+
+            return View();
+        }
+    }
+}
