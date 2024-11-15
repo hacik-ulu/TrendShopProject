2057650b41f7f29ad22df600599d71bb9215c149
Haçik Ulu <hacikd2@gmail.com>
2024-11-15 15:17:27 +0300

    ResourceOwnerPasswordTokenHandler editing.

---
+++ Frontends/TrendShop.WebUI/Handlers/ResourceOwnerPasswordTokenHandler.cs
@@ -0,0 +1,46 @@
+﻿
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using System.Net;
+using System.Net.Http.Headers;
+using TrendShop.WebUI.Services.Interfaces;
+
+namespace TrendShop.WebUI.Handlers
+{
+    public class ResourceOwnerPasswordTokenHandler : DelegatingHandler
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IIdentityService _identityService;
+
+        public ResourceOwnerPasswordTokenHandler(IIdentityService identityService)
+        {
+            _identityService = identityService;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            // Giriş yapan kullanıcının tokenının alınması.
+            var accessToken = await _httpContextAccessor.HttpContext.GetTokenAsync(OpenIdConnectParameterNames.AccessToken);
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken) // tokenın geçerliliği kontrol ediliyor.
+            var response = await base.SendAsync(request, cancellationToken);
+
+            if (response.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                var tokenResponse = await _identityService.GetRefreshToken();
+
+                if (tokenResponse != null)
+                {
+                    request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken) // tokenın geçerliliği kontrol ediliyor.
+                    response = await base.SendAsync(request, cancellationToken);
+                }
+            }
+
+            if (response.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                // hata mesajı dön
+            }
+            return response;
+
+        }
+    }
+}
