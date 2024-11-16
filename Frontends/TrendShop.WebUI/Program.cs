771223d54b05be60b59c33937713e5b4a9214101
Haçik Ulu <hacikd2@gmail.com>
2024-11-16 16:42:18 +0300

    Program.cs edited.

--- Frontends/TrendShop.WebUI/Program.cs
+++ Frontends/TrendShop.WebUI/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Localization;
 using System.Globalization;
 using TrendShop.WebUI.Handlers;
+using TrendShop.WebUI.Services.CatalogServices.CategoryServices;
 using TrendShop.WebUI.Services.Concrete;
 using TrendShop.WebUI.Services.Interfaces;
 using TrendShop.WebUI.Settings;
@@ -46,6 +47,11 @@ builder.Services.AddHttpClient<IUserService, UserService>(opt =>
 {
     opt.BaseAddress = new Uri(values.IdentityServerUrl);
 }).AddHttpMessageHandler<ResourceOwnerPasswordTokenHandler>();
+
+builder.Services.AddHttpClient<ICategoryService, CategoryService>(opt =>
+{
+    opt.BaseAddress = new Uri($"{values.OcelotUrl}/{values.Catalog.Path}");
+});
 
 builder.Services.AddHttpClient();
 builder.Services.AddControllersWithViews();
