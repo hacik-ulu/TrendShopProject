e33f31b16ea91b0d65acae1eaa338f52fdf53809
Haçik Ulu <hacikd2@gmail.com>
2024-11-19 13:49:10 +0300

    UI editing.

--- Frontends/TrendShop.WebUI/Controllers/DefaultController.cs
+++ Frontends/TrendShop.WebUI/Controllers/DefaultController.cs
@@ -6,6 +6,9 @@ namespace TrendShop.WebUI.Controllers
     {
         public IActionResult Index()
         {
+            ViewBag.directory1 = "TrendShop";
+            ViewBag.directory2 = "Ana Sayfa";
+            ViewBag.directory3 = "Ürün Listesi";
             return View();
         }
     }
---
+++ Frontends/TrendShop.WebUI/ViewComponents/UILayoutViewComponents/_DirectoryAlertComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrendShop.WebUI.ViewComponents.UILayoutViewComponents
+{
+    public class _DirectoryAlertComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
