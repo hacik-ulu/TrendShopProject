a4ede08d230e6fc19e4a726cb86ce737004e8f68
Haçik Ulu <hacikd2@gmail.com>
2024-11-20 16:05:59 +0300

    Basket editing.

--- Frontends/TrendShop.WebUI/Services/BasketServices/BasketService.cs
+++ Frontends/TrendShop.WebUI/Services/BasketServices/BasketService.cs
@@ -4,19 +4,58 @@ namespace TrendShop.WebUI.Services.BasketServices
 {
     public class BasketService : IBasketService
     {
+        private readonly HttpClient _httpClient;
+        public BasketService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task AddBasketItem(BasketItemDto basketItemDto)
+        { //Any --> en az bir öğe içerip içermediğini kontrol eder.
+
+            var values = await GetBasket(); // sepet getirilecek.
+            if (values != null) // sepetin var olduğunda dair kontrol
+            {
+                // Aynı ürün
+                if (!values.BasketItems.Any(x => x.ProductID == basketItemDto.ProductID))
+                {
+                    values.BasketItems.Add(basketItemDto);
+                }
+                else
+                {
+                    values = new BasketTotalDto();
+                    values.BasketItems.Add(basketItemDto);
+                }
+
+            }
+            await SaveBasket(values);
+        }
+
         public Task DeleteBasket(string userId)
         {
             throw new NotImplementedException();
         }
 
-        public Task<BasketTotalDto> GetBasketTotal(string userId)
+        public async Task<BasketTotalDto> GetBasket() // userId token'dan gelecek.
         {
-            throw new NotImplementedException();
+            var responseMessage = await _httpClient.GetAsync("baskets");
+            var values = await responseMessage.Content.ReadFromJsonAsync<BasketTotalDto>();
+            return values;
+
         }
 
-        public Task SaveBasket(BasketTotalDto basketTotalDto)
+        public async Task<bool> RemoveBasketItem(string productId)
         {
-            throw new NotImplementedException();
+            var values = await GetBasket();
+            var deletedItem = values.BasketItems.FirstOrDefault(x => x.ProductID == productId);
+            var result = values.BasketItems.Remove(deletedItem);
+            await SaveBasket(values);
+            return true;
+        }
+
+        public async Task SaveBasket(BasketTotalDto basketTotalDto)
+        {
+            await _httpClient.PostAsJsonAsync<BasketTotalDto>("baskets", basketTotalDto);
         }
     }
 }
--- Frontends/TrendShop.WebUI/Services/BasketServices/IBasketService.cs
+++ Frontends/TrendShop.WebUI/Services/BasketServices/IBasketService.cs
@@ -4,8 +4,10 @@ namespace TrendShop.WebUI.Services.BasketServices
 {
     public interface IBasketService
     {
-        Task<BasketTotalDto> GetBasketTotal(string userId); // Sepete veri getirmek.
-        Task SaveBasket(BasketTotalDto basketTotalDto); // Sepetteki değişiklikleri kaydetmek.
-        Task DeleteBasket(string userId);
+        Task<BasketTotalDto> GetBasket(); // userId'ye göre sepetin içeriği(sepetin tümünü) getirilecek.
+        Task SaveBasket(BasketTotalDto basketTotalDto); // Sepetteki değişiklikler kaydedilecek.(Card Summary kısmı)
+        Task DeleteBasket(string userId); // Komple sepetin tamamı silinecek.
+        Task AddBasketItem(BasketItemDto basketItemDto); // Sepet içerisine öğe eklenecek.
+        Task<bool> RemoveBasketItem(string productId); // Sepetteki öğelerin remove/silme işlemi için kullanılacak.
     }
 }
