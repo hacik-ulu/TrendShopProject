ba4f49104edacfafe3d614d7f49de95c43019ac6
Haçik Ulu <hacikd2@gmail.com>
2024-11-24 12:40:00 +0300

    Order pages editing.

---
+++ Frontends/TrendShop.WebUI/Controllers/OrderController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrendShop.WebUI.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.directory1 = "TrendShop";
+            ViewBag.directory2 = "İletişim";
+            ViewBag.directory3 = "Sipariş İşlemleri";
+            return View();
+        }
+    }
+}
