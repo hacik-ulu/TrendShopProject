62a8dab790978c800a37e86477fb7ad7c34d62fb
Haçik Ulu <hacikd2@gmail.com>
2024-11-26 15:01:30 +0300

    User Area added.

---
+++ Frontends/TrendShop.WebUI/Areas/User/Controllers/LogOutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrendShop.WebUI.Areas.User.Controllers
+{
+    public class LogOutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
